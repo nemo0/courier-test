791b41ca86c56aaf8427ff903063b6c9d05d4e8a
nemo0 <subhachanda88@gmai.com>
2022-09-03 13:04:16 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { CourierProvider } from '@trycourier/react-provider';
+import { Toast } from '@trycourier/react-toast';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const CLIENT_KEY = process.env.COURIER_CLIENT_KEY;
+  return (
+    <CourierProvider clientKey={CLIENT_KEY} userId={'test'}>
+      <Toast />
+      <Component {...pageProps} />
+    </CourierProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
